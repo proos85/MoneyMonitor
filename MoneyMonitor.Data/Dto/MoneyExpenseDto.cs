b65a71a9a6e65869a8bae7b82ed608fcc1cc5551
Patrick Roos <patrick@point42.nl>
2018-03-31 00:18:58 +0200

    Azure function toegevoegd

---
+++ MoneyMonitor.Data/Dto/MoneyExpenseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace MoneyMonitor.Data.Dto
+{
+    public class MoneyExpenseDto
+    {
+        public string NameExpense { get; set; } = string.Empty;
+        public ExpenseTypes TypeExpense { get; set; }
+        public double ValueExpense { get; set; }
+    }
+
+    public enum ExpenseTypes
+    {
+        Fixed,
+        Variable,
+        Charity
+    }
+}
